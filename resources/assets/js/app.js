fc1ce3ca1d59af2c08bb76db2d3c179e08ec3872
Peter Lightbody <pet1330@gmail.com>
2017-08-10 10:10:11 +0100

    Fixed mix compile

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,9 +7,9 @@ require('./AdminLTE');
 
 Vue.component('flash', require('./components/Flash.vue'));
 Vue.component('click-confirm', require('./components/ClickConfirm.vue'));
-Vue.component('toggle', require('./components/ToggleButton.vue'));
-Vue.component('simple-toggle', require('./components/simpleToggle.vue'));
-Vue.component('edit-modal', require('./components/EditModal.vue'));
+// Vue.component('toggle', require('./components/ToggleButton.vue'));
+// Vue.component('simple-toggle', require('./components/simpleToggle.vue'));
+// Vue.component('edit-modal', require('./components/EditModal.vue'));
 Vue.component('bug-report', require('./components/BugReport.vue'));
 
 window.flash = function(message) { window.events.$emit('flash', message); };
