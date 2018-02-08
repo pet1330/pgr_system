5a6457ff41cb6cd9941b739ff3cbc04e4f21c698
Peter Lightbody <pet1330@gmail.com>
2018-02-08 23:33:41 +0000

    Adding DO reference to footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
   <div class="pull-right hidden-xs">
-    Version: {{ $app_version }}
+    Proudly hosted on <a href="https://m.do.co/c/e827a0190785">Digital Ocean.</a>
+    <small>Version:{{ $app_version }}</small>
   </div>
-  <p>Developed by <a href="https://plightbody.com/" target="_blank">Peter Lightbody</a> &copy; {{ date("Y") }}</p>
+  <p>Developed by <a href="https://plightbody.com/" target="_blank">Peter Lightbody</a> &copy; {{ date("Y") }}.</p>
 </footer>
