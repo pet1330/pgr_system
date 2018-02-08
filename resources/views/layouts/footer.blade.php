e74f5d896728e8ec36c6516f955509397ed2dbc9
Peter Lightbody <pet1330@gmail.com>
2018-02-08 23:55:36 +0000

    protect against window.opener attacks

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="main-footer">
   <div class="pull-right hidden-xs">
-    Proudly hosted on <a href="https://m.do.co/c/e827a0190785">Digital Ocean.</a>
-    <small>Version:{{ $app_version }}</small>
+    <a href="https://m.do.co/c/e827a0190785" target="_blank" rel="noopener noreferrer">Proudly hosted on Digital Ocean</a>
   </div>
-  <p>Developed by <a href="https://plightbody.com/" target="_blank">Peter Lightbody</a> &copy; {{ date("Y") }}.</p>
+  <p>Developed by <a href="https://plightbody.com/" target="_blank" rel="noopener noreferrer">Peter Lightbody</a> &copy; {{ date("Y") }}.</p>
 </footer>
