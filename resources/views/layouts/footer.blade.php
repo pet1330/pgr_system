3ba8a83bda29d73b2441c276960fa19336d9659c
Peter Lightbody <pet1330@gmail.com>
2018-02-09 00:10:46 +0000

    moved link

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
   <div class="pull-right hidden-xs">
-    <a href="https://m.do.co/c/e827a0190785" target="_blank" rel="noopener noreferrer">Proudly hosted on Digital Ocean</a>
+    Proudly hosted on <a href="https://m.do.co/c/e827a0190785" target="_blank" rel="noopener noreferrer">Digital Ocean</a>
   </div>
   <p>Developed by <a href="https://plightbody.com/" target="_blank" rel="noopener noreferrer">Peter Lightbody</a> &copy; {{ date("Y") }}.</p>
 </footer>
