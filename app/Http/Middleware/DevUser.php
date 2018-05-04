aea859c66707ff9b07136234b6f413e32d70a0da
Peter Lightbody <pet1330@gmail.com>
2018-05-04 22:04:15 +0100

    Removes clutter from routes

---
+++ app/Http/Middleware/DevUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class DevUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if(\App::environment()==="local" && !auth()->check()) {
+            $hack_user = getenv('HACK_USER');
+            if ( !empty($hack_user) ) {
+                auth()->loginUsingId(\App\Models\Admin::whereUniversityId($hack_user)->first()->id);
+            }
+        }
+
+        return $next($request);
+    }
+}
---
+++ app/Http/Middleware/RoutePrefixProxy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class RoutePrefixProxy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if( config('app.proxy_url') )
+            \URL::forceRootUrl(config('app.proxy_url'));
+        if( config('app.proxy_schema') )
+            \URL::forceRootUrl(config('app.proxy_schema'));
+        return $next($request);
+    }
+}
